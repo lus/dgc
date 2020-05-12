40f64f80692714a3962424c46ff32afac3d4621f
Lukas SP <lukaesebrot@gmail.com>
2020-05-12 21:09:44 +0200

    Implement context structure

---
+++ context.go
@@ -0,0 +1,10 @@
+package dgc
+
+import "github.com/bwmarrin/discordgo"
+
+// Ctx represents the context for a command event
+type Ctx struct {
+	Session   *discordgo.Session
+	Event     *discordgo.MessageCreate
+	Arguments *Arguments
+}
