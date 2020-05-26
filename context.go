01c2c9f1d81f808b678b8bed7c5da340d498b395
Lukas SP <lukaesebrot@gmail.com>
2020-05-26 19:59:24 +0200

    Add respond methods to context

--- context.go
+++ context.go
@@ -14,3 +14,24 @@ type Ctx struct {
 
 // ExecutionHandler represents a handler for a context execution
 type ExecutionHandler func(*Ctx)
+
+// RespondText responds with the given text message
+func (ctx *Ctx) RespondText(text string) error {
+	_, err := ctx.Session.ChannelMessageSend(ctx.Event.ChannelID, text)
+	return err
+}
+
+// RespondEmbed responds with the given embed message
+func (ctx *Ctx) RespondEmbed(embed *discordgo.MessageEmbed) error {
+	_, err := ctx.Session.ChannelMessageSendEmbed(ctx.Event.ChannelID, embed)
+	return err
+}
+
+// RespondTextEmbed responds with the given text and embed message
+func (ctx *Ctx) RespondTextEmbed(text string, embed *discordgo.MessageEmbed) error {
+	_, err := ctx.Session.ChannelMessageSendComplex(ctx.Event.ChannelID, &discordgo.MessageSend{
+		Content: text,
+		Embed:   embed,
+	})
+	return err
+}
